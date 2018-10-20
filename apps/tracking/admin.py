c615312024bdf7dde13e37c12704ebd0bd3f979b
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-10-20 17:54:09 -0300

    Add Employee, Task, Project models

--- apps/tracking/admin.py
+++ apps/tracking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Employee, Task, Project
 
-# Register your models here.
+
+admin.site.register(Employee)
+admin.site.register(Task)
+admin.site.register(Project)
--- apps/tracking/models.py
+++ apps/tracking/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    description = models.CharField(max_length=120)
+    hours_estimated = models.IntegerField()
+    hours_spent = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.description} ({self.project})'
+
+
+class Employee(models.Model):
+    assigned_projects = models.ManyToManyField(Task, blank=True)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
